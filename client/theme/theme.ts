74b3d821e4f55880cb2f37f0211ac338094752a7
shambolic-samreeeed <samreedmaharjan1899@gmail.com>
2025-06-29 13:02:28 +0545

    added themes

---
+++ client/theme/theme.ts
@@ -0,0 +1,20 @@
+// theme/theme.js
+export const LightTheme = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    text: "#000000",
+    card: "#F5F5F5",
+    primary: "#6200ee",
+  },
+};
+
+export const DarkTheme = {
+  dark: true,
+  colors: {
+    background: "#000000",
+    text: "#FFFFFF",
+    card: "#1c1c1c",
+    primary: "#BB86FC",
+  },
+};
