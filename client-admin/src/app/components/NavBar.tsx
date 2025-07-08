52b45c319feaf3cc19efa9c56c320263d641add8
shambolic-samreeeed <samreedmaharjan1899@gmail.com>
2025-07-08 10:18:56 +0545

    adding nav bar

--- client-admin/src/app/components/NavBar.tsx
+++ client-admin/src/app/components/NavBar.tsx
@@ -1,33 +1,17 @@
 "use client";
-
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React from "react";
+import Link from "next/link";
 
 const NavBar = () => {
-  const router = useRouter();
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    router.replace("/login");
-  };
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      router.replace("/login");
-    }
-  }, [router]);
-
   return (
-    <div className="flex justify-between items-center bg-[#3C3C3C] text-white p-2">
-      <div>Admin Panel</div>
-      <div className="flex items-center">
-        <ul className="flex gap-4 items-center">
-          <li>Late Slips</li>
-          <li>Students</li>
-          <li>Schedules</li>
-          <button onClick={handleLogout}>Logout</button>
-        </ul>
+    <div>
+      <div>
+        <div>Logo</div>
+        <div>
+          <Link href={"/lateslip"}>Late Slips</Link>
+          <Link href={"/students"}>Students</Link>
+          <Link href={"/login"}>Schedules</Link>
+        </div>
       </div>
     </div>
   );
